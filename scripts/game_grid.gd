98b50276ac9b2cc57165cfca8dfb745d40a4ad56
Tim Slippy, voxelv <slippytim@gmail.com>
2018-09-30 15:38:41 -0700

    more movement testing

--- scripts/game_grid.gd
+++ scripts/game_grid.gd
@@ -53,28 +53,35 @@ func request_move(grid_object, direction):
 	var coord = world_to_map(grid_object.get_cellv_test_pos())
 	print("cellv coord: ", coord)
 	
-	# Check if can move
-	match(direction):
-		utl.DIR_N:
-			for i in range(grid_object.width):
-				print("check: ", coord.x + i, ", ", coord.y - 1)
-				if tilemap.get_cell(coord.x + i, coord.y - 1) != utl.GRID_NONE:
-					can_move = false
-		utl.DIR_E:
-			for i in range(grid_object.height):
-				print("check: ", coord.x + grid_object.width, ", ", coord.y + i )
-				if tilemap.get_cell(coord.x + grid_object.width, coord.y + i) != utl.GRID_NONE:
-					can_move = false
-		utl.DIR_S:
-			for i in range(grid_object.width):
-				print("check: ", coord.x + i, ", ", coord.y + grid_object.height)
-				if tilemap.get_cell(coord.x + i, coord.y + grid_object.height) != utl.GRID_NONE:
-					can_move = false
-		utl.DIR_W:
-			for i in range(grid_object.height):
-				print("check: ", coord.x - 1, ", ", coord.y + i)
-				if tilemap.get_cell(coord.x - 1, coord.y + i) != utl.GRID_NONE:
-					can_move = false
+	# Check if out-of-bounds
+	if     (direction == utl.DIR_W and (coord.x - 1) < 0) \
+		or (direction == utl.DIR_N and (coord.y - 1) < 0) \
+		or (direction == utl.DIR_E and (coord.x + grid_object.width) >= utl.TILES_PER_SIDE) \
+		or (direction == utl.DIR_S and (coord.y + grid_object.height) >= utl.TILES_PER_SIDE):
+			can_move = false
+	else:
+		# Check if can move in-bounds
+		match(direction):
+			utl.DIR_N:
+				for i in range(grid_object.width):
+					print("check: ", coord.x + i, ", ", coord.y - 1)
+					if tilemap.get_cell(coord.x + i, coord.y - 1) != utl.GRID_NONE:
+						can_move = false
+			utl.DIR_E:
+				for i in range(grid_object.height):
+					print("check: ", coord.x + grid_object.width, ", ", coord.y + i )
+					if tilemap.get_cell(coord.x + grid_object.width, coord.y + i) != utl.GRID_NONE:
+						can_move = false
+			utl.DIR_S:
+				for i in range(grid_object.width):
+					print("check: ", coord.x + i, ", ", coord.y + grid_object.height)
+					if tilemap.get_cell(coord.x + i, coord.y + grid_object.height) != utl.GRID_NONE:
+						can_move = false
+			utl.DIR_W:
+				for i in range(grid_object.height):
+					print("check: ", coord.x - 1, ", ", coord.y + i)
+					if tilemap.get_cell(coord.x - 1, coord.y + i) != utl.GRID_NONE:
+						can_move = false
 	print("CAN MOVE? ", can_move)
 
 	# If can_move:
