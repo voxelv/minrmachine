0f2fc4ce39d10da51216f1b6747f026d00168c4a
Tim Slippy, voxelv <slippytim@gmail.com>
2018-10-06 16:18:45 -0700

    testing adding tiles via script

--- scripts/cell.gd
+++ scripts/cell.gd
@@ -1,6 +1,21 @@
 extends 'grid_object.gd'
 
+onready var organelle_grid = get_node("draw/organelles")
+onready var e_grid = get_node("draw/e_grid")
+
+var count = 0
 
 func _ready():
 	pass
+
+func _process(delta):
+	if Input.is_action_just_pressed("TEST"):
+		add_wire(Vector2(count % 5, count / 5))
+		e_grid.update_bitmask_region()
+		count += 1
+
+
+func add_wire(vcoord):
+	e_grid.set_cell(vcoord.x, vcoord.y, 0, false, false, false, Vector2(0, 0))
+
 
